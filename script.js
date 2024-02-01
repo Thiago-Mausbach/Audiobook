079ff620e3ac972b00c0adc85ad906ba9f7e424f
Thiago-Mausbach <80917439+Thiago-Mausbach@users.noreply.github.com>
2024-02-01 09:40:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+const botaoPlayPause = document.getElementById("play-pause");
+const audioCapitulo = document.getElementById("audio-capitulo");
+const botaoAvancar = document.getElementById("proximo");
+const botaoVoltar = document.getElementById("anterior");
+const nomeCapitulo = document.getElementById("capitulo");
+const numeroCapitulos = 10;
+let tocando = 0
+let capituloAtual = 1;
+
+function tocarFaixa () {
+    botaoPlayPause.classList.remove('bi-play')
+    botaoPlayPause.classList.add('bi-pause-circle')
+    audioCapitulo.play();
+    tocando = 1;
+}
+
+function pausarFaixa() {
+    botaoPlayPause.classList.remove('bi-pause-circle')
+    botaoPlayPause.classList.add('bi-play')
+    audioCapitulo.pause();
+    tocando = 0;
+}
+
+function proximaFaixa() {
+    if (capituloAtual === numeroCapitulos) {
+        capituloAtual = 1;
+    } else {
+        capituloAtual = capituloAtual + 1;
+    }
+
+    audioCapitulo.src = "./books/dom-casmurro/" + capituloAtual + ".mp3";
+    tocarFaixa();
+    tocando = 1;
+    trocarNome();
+}
+
+function voltarFaixa() {
+    if (capituloAtual === 1) {
+        capituloAtual = numeroCapitulos;
+    } else {
+        capituloAtual = capituloAtual - 1;
+    }
+
+    audioCapitulo.src = "./books/dom-casmurro/" + capituloAtual + ".mp3";
+    tocarFaixa();
+    tocando = 1;
+    trocarNome();
+}
+//tocando == 0 ? tocarFaixa() : pausarFaixa();?
+function tocarPausar () {
+    if (tocando == 0) {
+        tocarFaixa();
+    } else {
+        pausarFaixa();
+    }
+}
+
+function trocarNome () {
+    nomeCapitulo.innerText = 'Capítulo ' + capituloAtual;
+}
+
+botaoPlayPause.addEventListener('click', tocarPausar);
+botaoAvancar.addEventListener('click', proximaFaixa);
+botaoVoltar.addEventListener('click', voltarFaixa);
